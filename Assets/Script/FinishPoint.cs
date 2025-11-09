7e2b892f333b3ad517919b95957eac949ee4520b
shibangoo <minhhoanghcm10@gmail.com>
2025-11-10 00:39:51 +0700

    Chuyển scene

---
+++ Assets/Script/FinishPoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FinishPoint : MonoBehaviour
+{    
+   private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // Gọi hàm hoàn thành cấp độ từ GameManager
+           SceneController.Instance.NextLevel();
+        }
+    }
+}
---
+++ Assets/Script/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance;
+    
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
